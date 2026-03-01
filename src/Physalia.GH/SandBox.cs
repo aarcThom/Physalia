7be1dba5ae6e577cc05427caaadcf4b74d933458
aarcThom <robertthomasgaudin@gmail.com>
2026-03-01 13:15:33 -0800

    Get Python3 loading to work.

--- src/Physalia.GH/SandBox.cs
+++ src/Physalia.GH/SandBox.cs
@@ -1,11 +1,12 @@
 using Grasshopper.Kernel;
-using System;
 using Rhino.Runtime.Code;
 using Rhino.Runtime.Code.Execution;
+using System;
+using System.Linq;
 
 namespace Physalia.GH
 {
-    public class Physalia : GH_Component
+    public class SandBox : GH_Component
     {
         /// <summary>
         /// Each implementation of GH_Component must provide a public 
@@ -14,8 +15,8 @@ namespace Physalia.GH
         /// Subcategory the panel. If you use non-existing tab or panel names, 
         /// new tabs/panels will automatically be created.
         /// </summary>
-        public Physalia()
-          : base("Physalia Editor", "Phy",
+        public SandBox()
+          : base("Sandbox", "Phy",
             "An AI assisted code editor for Grasshopper",
             "Physalia", "Core")
         {
@@ -47,47 +48,7 @@ namespace Physalia.GH
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            // 1. The ultra-strict Rhino 8 shebang (notice the space!)
-            string pythonCode =
-                "#! python 3" + Environment.NewLine +
-                "import sys" + Environment.NewLine +
-                "import math" + Environment.NewLine +
-                Environment.NewLine +
-                "calculated_value = input_number * math.pi" + Environment.NewLine +
-                "python_version = sys.version" + Environment.NewLine;
 
-            // 2. Set up our test input variable
-            int userNumber = 10;
-
-            // 3. Create the Execution Context
-            var ctx = new Rhino.Runtime.Code.Execution.RunContext
-            {
-                AutoApplyParams = true,
-                Inputs = { ["input_number"] = userNumber },
-                Outputs = { ["calculated_value"] = null, ["python_version"] = null }
-            };
-
-            try
-            {
-                // 4. Create and run the code using the single, proven argument!
-                var code = Rhino.Runtime.Code.RhinoCode.CreateCode(pythonCode);
-                code.Run(ctx);
-
-                // 5. Extract the outputs back into C# variables
-                if (ctx.Outputs.TryGet("calculated_value", out object resultVal))
-                {
-                    DA.SetData(0, resultVal); // Output back to GH
-                }
-
-                if (ctx.Outputs.TryGet("python_version", out object versionStr))
-                {
-                    DA.SetData(1, versionStr); // Output back to GH
-                }
-            }
-            catch (Exception ex)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Python Error: {ex.Message}");
-            }
         }
 
         /// <summary>
@@ -104,5 +65,6 @@ namespace Physalia.GH
         /// that use the old ID will partially fail during loading.
         /// </summary>
         public override Guid ComponentGuid => new Guid("24AA372D-00AD-4350-9B1D-AE180549C4D3");
+       
     }
 }
---
+++ src/Physalia.GH/StartLanguage.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Rhino;
+
+namespace Physalia.GH
+{
+    public static class StartLanguage
+    {
+        /// <summary>
+        /// Loads Python3 by writing a temp .py file and loading it into the Rhino Script editor
+        /// </summary>
+        public static void LoadPython()
+        {
+            string tempScript = Path.Combine(Path.GetTempPath(), "my_script.py");
+            File.WriteAllText(tempScript, "#! python 3\nimport sys\nprint(sys.version)");
+            RhinoApp.RunScript($"-_ScriptEditor _R \"{tempScript}\"", false);
+        }
+    }
+}

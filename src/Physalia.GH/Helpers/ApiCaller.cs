b1ad0f5156aa3e20f5d0cda14348c77036c68a04
aarcThom <thomas@aarc.io>
2026-03-02 13:09:22 -0800

    Start working through code to refactor weekend rush

--- src/Physalia.GH/Helpers/ApiCaller.cs
+++ src/Physalia.GH/Helpers/ApiCaller.cs
@@ -9,6 +9,11 @@ namespace Physalia.GH.Helpers;
 
 public class ApiCaller
 {
+    /* WHY NO CONSTRUCTOR?
+     * IN C# if you create a class without a constructor, the compiler automatically generates
+     * default constructor with no arguments. (see: https://en.wikipedia.org/wiki/Default_constructor)
+     * If this is done, then fields are set to default values. _waiting below is set to the default, false. */
+
     private bool _waiting;
 
     public bool IsWaiting => _waiting;
@@ -29,17 +34,23 @@ public class ApiCaller
         Action<ScriptResponse> onSuccess,
         Action<string> onError)
     {
+        // make sure that we can't send another request before the current one is totally resolved
         if (_waiting) return;
         _waiting = true;
 
+        // delegate the below code to a background worker
+        // NOTE: COULD THIS JUST BE ASYNC / AWAIT?
         Task.Run(async () =>
         {
             try
             {
                 var resolver = new ApiKeyResolver(keysPath);
-                var apiKey = resolver.GetKey("Claude Code");
 
+                // TO DO ----- MAKE THIS PROVIDER AGNOSTIC
+                var apiKey = resolver.GetKey("Claude Code");
                 var provider = new AnthropicProvider();
+                // ----------------------------------------
+
                 var client = new PhysaliaClient(provider, apiKey);
 
                 var result = await client.GenerateScriptAsync(prompt);
---
+++ src/architecture.md
@@ -0,0 +1,4 @@
+## What happens when a component runs?
+
+### An ApiCaller Object is Initialized
+This object is what grabs the key with the ApiKeyResolver and then sends the request 
